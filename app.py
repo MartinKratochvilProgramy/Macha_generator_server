92aa58a7efa5b76aa120955ac914881476c807ea
MartinKratochvilProgramy <martvil96@gmail.com>
2023-02-01 14:00:47 +0100

    added vercel build

--- app.py
+++ app.py
@@ -8,20 +8,6 @@ from model.Dataset import Dataset
 app = Flask(__name__)
 CORS(app)
 
-dataset = Dataset()
-model = Model(
-    vocab_size=dataset.get_vocab_length(),
-    embedding_dim=256,
-    rnn_units=512
-    )
-model.load_weights(tf.train.latest_checkpoint('./model/training_checkpoints'))
-one_step_model = OneStep(
-    model = model, 
-    chars_from_ids = dataset.chars_from_ids, 
-    ids_from_chars = dataset.ids_from_chars,
-    temperature = 0.001
-)
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -48,18 +34,19 @@ def generate_text():
         return None
 
 
-# if __name__ == '__main__':
-#     dataset = Dataset()
-#     model = Model(
-#         vocab_size=dataset.get_vocab_length(),
-#         embedding_dim=256,
-#         rnn_units=512
-#         )
-#     model.load_weights(tf.train.latest_checkpoint('./model/training_checkpoints'))
-#     one_step_model = OneStep(
-#         model = model, 
-#         chars_from_ids = dataset.chars_from_ids, 
-#         ids_from_chars = dataset.ids_from_chars,
-#         temperature = 0.001
-#     )
-#     app.run(debug=True, host="localhost", port=3000)
+if __name__ == '__main__':
+    dataset = Dataset()
+    model = Model(
+        vocab_size=dataset.get_vocab_length(),
+        embedding_dim=256,
+        rnn_units=512
+        )
+    model.load_weights(tf.train.latest_checkpoint('./model/training_checkpoints'))
+    one_step_model = OneStep(
+        model = model, 
+        chars_from_ids = dataset.chars_from_ids, 
+        ids_from_chars = dataset.ids_from_chars,
+        temperature = 0.001
+    )
+    # app.run(debug=True, host="localhost", port=3000)
+    app.run()
